b51e3edde44600bffc02041141b836242ddcb5d0
LEE, TAEGON <taegon1998@gmail.com>
2022-11-09 15:46:14 +0900

    taegon

---
+++ src/main/java/beeges/onepostershop/OnepostershopApplication.java
@@ -0,0 +1,13 @@
+package beeges.onepostershop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnepostershopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnepostershopApplication.class, args);
+	}
+
+}

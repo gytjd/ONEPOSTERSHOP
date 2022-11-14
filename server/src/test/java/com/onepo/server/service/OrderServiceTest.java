6c4b9bab3cd74d5a3a217603ccb531ac40a67110
gytjd <hys339631@gmail.com>
2022-11-14 18:21:02 +0900

    11/14 BLUE

---
+++ server/src/test/java/com/onepo/server/service/OrderServiceTest.java
@@ -0,0 +1,13 @@
+package com.onepo.server.service;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OrderServiceTest {
+
+}

a89592becf217b7ecf7587d0409e7ca40aaf86c6
LEE, TAEGON <taegon1998@gmail.com>
2022-11-14 15:24:11 +0900

    11/14 taegon

---
+++ server/src/main/java/com/onepo/server/domain/News.java
@@ -0,0 +1,26 @@
+package com.onepo.server.domain;
+
+import lombok.Getter;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+public class News {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String title;
+
+    private LocalDateTime localDateTime;
+
+    private String content;
+
+    private MultipartFile multipartFile;
+}

28aa62e50af5c3741c90bea763e405d3087ce39f
gytjd <hys339631@gmail.com>
2022-11-16 01:32:12 +0900

    11/16 WishService Method 형식으로 코드 변환

--- server/src/main/java/com/onepo/server/service/WishService.java
+++ server/src/main/java/com/onepo/server/service/WishService.java
@@ -11,6 +11,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class WishService {
@@ -26,41 +28,80 @@ public class WishService {
     public void createCart(Member member) {
         Wish wish=Wish.createCart(member);
 
-        wishRepository.save(wish);
+        save_wish(wish);
 
     }
     @Transactional
     public Long addCart(Member member,Item item,int count) {
 
-        Wish wish = wishRepository.findByMemberId(member.getId()); // member 가 장바구니 한거 있나 확인
+        Wish wish = findByMemberId(member.getId()); // member 가 장바구니 한거 있나 확인
 
         if (wish==null) { // 여기서 없으면 그냥 새로 만들어서 save
             wish=Wish.createCart(member);
-            wishRepository.save(wish);
+            save_wish(wish);
         }
 
         Item findItem = itemRepository.findOne(item.getId()); // 아이템 찾고
-        WishItem findWish = wishItemRepository.findByWishIdAndItemId(wish.getId(), findItem.getId());
+        WishItem findWish = findByWishIdAndItemId(wish.getId(), findItem.getId());
 
 
         if (findWish == null) {
             findWish=WishItem.cartItem(wish,item,count);
-            wishItemRepository.save(findWish);
+            save_wish_Item(findWish);
         }
         else {
-            WishItem update=findWish;
-            update.setWish(findWish.getWish());
-            update.setItem(findWish.getItem());
-            update.addCount(count);
-            update.setWishCount(update.getWishCount());
-
-            wishItemRepository.save(update);
+            WishItem update = updateWish(findWish, count);
+            save_wish_Item(update);
         }
 
         wish.setCount(wish.getCount()+count);
 
         return wish.getId();
     }
+
+
+    public WishItem updateWish(WishItem findWish,int count) {
+        WishItem update=findWish;
+        update.setWish(findWish.getWish());
+        update.setItem(findWish.getItem());
+        update.addCount(count);
+        update.setWishCount(update.getWishCount());
+
+        return update;
+    }
+
+    // WishRepository Service
+
+    public void save_wish(Wish wish) {
+        wishRepository.save(wish);
+    }
+
+    public Wish findByMemberId(Long id) {
+        return wishRepository.findByMemberId(id);
+    }
+
+    public Wish findWishById(Long id) {
+        return wishRepository.findWishById(id);
+    }
+
+    public Wish findWishByMemberId(Long id) {
+        return wishRepository.findWishByMemberId(id);
+    }
+
+    // WishItemRepository Service
+
+    public void save_wish_Item(WishItem wishItem) {
+        wishItemRepository.save(wishItem);
+    }
+
+    public WishItem findByWishIdAndItemId(Long wishId,Long itemId) {
+        return wishItemRepository.findByWishIdAndItemId(wishId,itemId);
+    }
+
+    public List<WishItem> findWishItemsByWishId(Long id) {
+        return wishItemRepository.findWishItemsByWishId(id);
+    }
+
 
 
 
